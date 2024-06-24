57602ca580ce011151f9e98815263cc147a92cb7
88vipProdev <nguyentunglam18091998abc@gmail.com>
2024-06-24 21:27:51 +0700

    route

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.get('/',(req,res)=>{
 
 })
 
+app.get('/abc',(req,res)=>{
+    res.send('hello world ! abc ')
+
+
+})
+
+app.get('/88vipPro',(req,res)=>{
+    res.send('<h1>lap trinh voi 88vippro</h1> ')
+
+
+})
 app.listen(port , ()=>{
     console.log(`Example app listening on port ${port}`)
 })
