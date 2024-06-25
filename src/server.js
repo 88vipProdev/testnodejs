afc05af358daff7ece84618cfcb9f4d79ca5eb73
88vipProdev <nguyentunglam18091998abc@gmail.com>
2024-06-25 10:04:28 +0700

    fix routes

--- src/server.js
+++ src/server.js
@@ -1,33 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express()
+const webRoutes = require('./routes/web');
+const configViewEngine = require('./config/viewEngine')
 const port = process.env.PORT || 8888 ;
 const hostname = process.env.HOST_NAME;
 require('dotenv').config()
+configViewEngine(app);
 
-// config template engine
-app.set('views',path.join(__dirname,'views')) ;
-app.set('view engine', 'ejs')
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/',(req,res)=>{
-    res.send('hello world ! ')
+app.use('/',webRoutes);
 
 
-})
-
-app.get('/abc',(req,res)=>{
-    res.send('hello world ! abc ')
-
-
-})
-
-app.get('/88vipPro',(req,res)=>{
-   res.render('sample.ejs')
-
-
-})
 app.listen(port , ()=>{
     console.log(`Example app listening on port ${port}`)
 })
