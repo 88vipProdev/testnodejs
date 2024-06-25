eeab894b870d5d516c3526903387b0d1d2963a41
88vipProdev <nguyentunglam18091998abc@gmail.com>
2024-06-25 08:31:20 +0700

    env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express()
-const port = 8081
+const port = process.env.PORT || 8888 ;
+const hostname = process.env.HOST_NAME;
+require('dotenv').config()
 
 // config template engine
 app.set('views',path.join(__dirname,'views')) ;
